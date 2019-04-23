1212c96eead4f8593bf9492f24c1cd569500f879
crewsz96 <crewsz96@vt.edu>
2019-04-23 19:26:24 -0400

    Updated for Discord.py 1.0.0

--- Dende.py
+++ Dende.py
@@ -22,84 +22,79 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------------')
-    await bot.change_presence(game=Game(name="Guarding Earth"))
+    await bot.change_presence(activity=Game(name="Guarding Earth"))
 
 
 #----------------------------------------------#
 @bot.command(name='hook',
             descripton='Answers the gang.',
-            aliases=['Hook'],
-            pass_context=True)
-async def hook(context):
+            aliases=['Hook'])
+async def hook(ctx):
 
-
-    await bot.say("***GANG GAWD***")
+    await ctx.send("***GANG GAWD***")
 #----------------------------------------------#
 
 @bot.command(name='lilgreen',
-            aliases=['LilGreen', 'Lilgreen'],
-            pass_context=True)
-async def lilgreen(context):
+            aliases=['LilGreen', 'Lilgreen'])
+async def lilgreen(ctx):
 
-
-    await bot.say("My name is **Dende**")
+    await ctx.send("My name is **Dende**")
 
 #----------------------------------------------#
 @bot.command(name='addrole',
-            aliases=['add', 'addRole', 'AddRole', 'Add'],
-            pass_context=True)
-async def addrole(context):
-    if context.message.channel.name != 'role-requests':
+            aliases=['add', 'addRole', 'AddRole', 'Add'])
+async def addrole(ctx):
+    if ctx.channel.name != 'role-requests':
         return
 
     valid_roles = ['Sync\'d with Nail', 'Ma Junior',
                     'NA', 'EU', 'Steam', 'PS4', 'Xbone']
-    entered_role = context.message.content.split(' ', 1)[1]
-    member = context.message.author
-    role = discord.utils.get(context.message.server.roles, name=entered_role)
+    msg = ctx.message
+    entered_role = ctx.message.content.split(' ', 1)[1]
+    member = ctx.author
+    role = discord.utils.get(ctx.guild.roles, name=entered_role)
 
     if role == None or role.name not in valid_roles:
-        msg = await bot.send_message(context.message.channel, "Invalid Role {0.message.author.mention}, roles you are able to add are: \n`\"Ma Junior\", \"Sync\'d with Nail\", \"NA\", \"EU\", \"Steam\", \"PS4\", \"Xbone\"`".format(context))
+        bot_msg = await ctx.send("Invalid Role {0.message.author.mention}, roles you are able to add are: \n`\"Ma Junior\", \"Sync\'d with Nail\", \"NA\", \"EU\", \"Steam\", \"PS4\", \"Xbone\"`".format(ctx))
     else:
-        await bot.add_roles(member, role)
-        msg = await bot.send_message(context.message.channel, 'I\'ve given {0.message.author.mention} the role **{1.name}**'.format(context, role))
+        await member.add_roles(role)
+        bot_msg = await ctx.send('I\'ve given {0.message.author.mention} the role **{1.name}**'.format(ctx, role))
 
     await asyncio.sleep(10)
-    await bot.delete_message(msg)
-    await bot.delete_message(context.message)
+    await msg.delete()
+    await bot_msg.delete()
 #--------------------------------------------------#
 
 @bot.command(name='removerole',
-            aliases=['Removerole', 'RemoveRole', 'removeRole','Rm', 'rm', 'rM', 'remove'],
-            pass_context=True)
-async def removerole(context):
-    if context.message.channel.name != 'role-requests':
+            aliases=['Removerole', 'RemoveRole', 'removeRole','Rm', 'rm', 'rM', 'remove'])
+async def removerole(ctx):
+    if ctx.channel.name != 'role-requests':
         return
 
-    entered_role = context.message.content.split(' ', 1)[1]
-    member = context.message.author
-    role = discord.utils.get(context.message.server.roles, name=entered_role)
+    msg = ctx.message
+    entered_role = ctx.message.content.split(' ', 1)[1]
+    member = ctx.author
+    role = discord.utils.get(ctx.guild.roles, name=entered_role)
 
     if role == None:
-        msg = await bot.send_message(context.message.channel, "Invalid Role {0.message.author.mention}, roles avaiable to remove are: \n'\"Ma Junior\" \"Sync\'d with Nail\", \"NA\", \"EU\", \"Steam\", \"PS4\", \"Xbone\"'".format(context))
-    elif role not in context.message.author.roles:
-        msg = await bot.send_message(context.message.channel, "{0.message.author.mention} You do not have this role.".format(context))
+        bot_msg = await ctx.send("Invalid Role {0.message.author.mention}, roles avaiable to remove are: \n`\"Ma Junior\" \"Sync\'d with Nail\", \"NA\", \"EU\", \"Steam\", \"PS4\", \"Xbone\"`".format(ctx))
+    elif role not in ctx.author.roles:
+        bot_msg = await ctx.send("{0.message.author.mention} You do not have this role.".format(ctx))
     else:
-        await bot.remove_roles(member, role)
-        msg = await bot.send_message(context.message.channel, 'I\'ve removed the role **{0.name}** from {1.message.author.mention} '.format(role, context))
+        await member.remove_roles(role)
+        bot_msg = await ctx.send('I\'ve removed the role **{0.name}** from {1.message.author.mention} '.format(role, ctx))
 
     await asyncio.sleep(10)
-    await bot.delete_message(msg)
-    await bot.delete_message(context.message)
+    await msg.delete()
+    await bot_msg.delete()
 
 #----------------------------------------------#
 # Kill command for the bot.
-@bot.command(name='stop',
-            pass_context=True)
-async def stop(context):
+@bot.command(name='stop')
+async def stop(ctx):
 
-    m = context.message.author
-    if m.server_permissions.administrator or "Fused With Kami" in m.roles:
+    role = discord.utils.get(ctx.guild.roles, name="Fused With Kami")
+    if m.guild_permissions.administrator or role in ctx.author.roles:
         await bot.close()
     else:
         return
@@ -109,10 +104,11 @@ async def stop(context):
             description='Gives the current uptime of the bot as Hour/Min/Sec',
             brief='Curent uptime of Dende',
             aliases=['up', 'Uptime', 'Up'])
-async def uptime():
+async def uptime(ctx):
+
     current_time = time.time()
     uptime = int(round(current_time - start_time))
-    await bot.say("Uptime: {}".format(str(datetime.timedelta(seconds=uptime))))
+    await ctx.send("Uptime: {}".format(str(datetime.timedelta(seconds=uptime))))
 
 #-------------------------------------------------------------#
 
@@ -120,9 +116,9 @@ async def uptime():
             description='Source code for Dende',
             brief='Source code for Dende',
             aliases=['sc', 'sourcecode'])
-async def source():
+async def source(ctx):
 
-    await bot.say("You want to see my source code? Anything for you Gohan :wink: https://github.com/crewsz96/Dende_Bot")
+    await ctx.send("You want to see my source code? Anything for you Gohan :wink: https://github.com/crewsz96/Dende_Bot")
 
 #-------------------------------------------------------------#
 
